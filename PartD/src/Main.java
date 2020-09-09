73ef543de886dd0f6abcd22bf0ee121283ff9079
NZWebDesign <65640337+NZWebDesign@users.noreply.github.com>
2020-09-09 14:30:08 +1200

    Done

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		System.out.println("Deviating from the assignemnt a bit here because this thing is stupid... :)");
 		System.out.println("My favourite mode of transportation is anything that gets you there");
 		System.out.println("The best day of the week is ... any day you don't have to use Git Hub");
+		System.out.println("Ok that's enough of this for now....");
 	}
 
 }
