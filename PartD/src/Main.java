65e5588584004ec56844eb2f71521ba0d374e33f
NZWebDesign <65640337+NZWebDesign@users.noreply.github.com>
2020-09-09 14:27:55 +1200

    possibly seems to be working now...

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("My favourite colour is blue");
 		System.out.println("Deviating from the assignemnt a bit here because this thing is stupid... :)");
 		System.out.println("My favourite mode of transportation is anything that gets you there");
+		System.out.println("The best day of the week is ... any day you don't have to use Git Hub");
 	}
 
 }
